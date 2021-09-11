4e5d2f7d389e526dd9f1e5c4a4787bb882857d08
nYCSTs <lucascandradex14@gmail.com>
2021-09-11 00:24:32 -0300

    Increase timeout to prevent bot spam abuse

--- twitch_bot.js
+++ twitch_bot.js
@@ -5,7 +5,10 @@ let startTime;
 let blank = false;
 const latestBans = {};
 const blankText = ' 󠀀󠀀󠀀󠀀󠀀󠀀';
-const channels = []
+const channels = [
+  'ablacs',
+  // 'forsen'
+]
 
 channels.map((ch) => latestBans[ch] = []);
 
@@ -17,8 +20,7 @@ const client = new tmi.Client({
   channels
 });
 
-
-client.on('message', onMessageHandler);
+setTimeout(() => client.on('message', onMessageHandler), 1000);
 client.on('connected', onConnectedHandler);
 client.on('ban', onBanUserHandler);
 
@@ -50,95 +52,81 @@ function startEDTimer(channel) {
   }, [3600100]);
 };
 
+function antiSpam() {
+  setTimeout(() => {}, 3000);
+}
+
 function onMessageHandler(channel, context, msg, self) {
   if (self || context['username'] === 'Supibot') { return; }
 
+  let message;
   const userMessage = msg.replace('󠀀', '').trim().split(' ').filter((x) => x !== '');
 
-  setTimeout(() => {
-    switch (userMessage.length) {
-      case 1:
-        // Ping
-        if (userMessage[0] === '*ping') {
-          const message = `@${context['display-name']} running for ${msToTime(new Date() - startTime)}.`;
-          client.say(channel, `${blank ? message : message.concat(blankText)}`);
-          blank = !blank;
-          return;
+  switch (userMessage.length) {
+    case 1:
+      // Ping
+      if (userMessage[0] === '*ping') {
+        message = `@${context['display-name']} Karen  running for ${msToTime(new Date() - startTime)}.`;
+      }
+      // Check current Channel
+      else if (userMessage[0] === '*latest') {
+        // Check if bans were registered
+        if (latestBans[channel.slice(1).toLowerCase()].length) {
+          message = `@${context['display-name']} the last banned users were: ${latestBans[channel.slice(1)].map((user) => `${user.username} (${msToTime(new Date() - user.time)} ago) `)}`
+        } else {
+          message = `@${context['display-name']} No bans were registered.`
         }
-        // Check current Channel
-        if (userMessage[0] === '*latest') {
-          // Check if bans were registered
-          if (latestBans[channel.slice(1).toLowerCase()].length) {
-            client.say(
-              channel,
-              `@${context['display-name']} the last banned users were: ${latestBans[channel.slice(1)].map((user) => `${user.username} (${msToTime(new Date() - user.time)} ago) `)}`
-            );
-            return;
+      }
+      // AYAYA
+      else if (userMessage[0] === 'AYAYA') {
+        message = `@${context['display-name']} AYAYA`;
+      }
+      // nyanPls
+      else if (userMessage[0] === 'nyanPls') {
+        message = '✨ nyanPls 🌸  ';
+      }
+      // cute chat
+      else if (userMessage[0] === '*cute') {
+        message = '🌸 ✌ ️ AYAYA ✨ ⣰⠟⢷⡀⣿⠄⣿⠘⢻⡟⠃⣿⠛⠛⠄⠄⣰⠟⢷⡀⣿⠄⣿⠄⢠⣿⠄⠛⣿⠛ ⣿⠄⠄⠄⣿⠄⣿⠄⢸⡇⠄⣿⠶⠶⠄⠄⣿⠄⠄⠄⣿⠶⣿⠄⣼⣈⡇⠄⣿⠄ ⠹⣦⡾⠁⠻⣤⠟⠄⢸⡇⠄⣿⣤⣤⠄⠄⠹⣦⡾⠁⣿⠄⣿⢠⡏⠉⢻⠄⣿⠄'
+      }
+      // paag
+      else if (userMessage[0] === '*pag') {
+        message = 'PagMan 🤘 ⣿⣯⣷⢈⣹⣿⠿⡟⢛⡛⢿⡟⠛⣿⣿⡟⠛⣿⣿⠟⠻⣿⡟⢛⡛⢿⡿⢛⡛⢿ ⣉⣛⠛⠉⠁⠄⠄⡇⢈⣁⣼⠁⠇⠸⡿⠠⠇⢹⡟⠰⠆⢻⠄⢿⡉⢹⡀⢾⡉⢹ ⡿⠌⠛⠢⣄⣀⣠⣧⣼⣿⣧⣼⣿⣤⣧⣼⣿⣤⣥⣾⣧⣬⣷⣬⣥⣼⣷⣤⣤⣼ '
+      }
+      // join raid
+      else if (userMessage[0] === '*join') {
+        message = '+join';
+      }
+      // force ed
+      else if (userMessage[0] === '*fed' && context['display-name'] === process.env.USER) {
+        clearInterval(timer);
+        client.say(channel, '+ed');
+        timer = startEDTimer(channel);
+        return;
+      }
+      break;
+    case 2:
+      // Registered bans on <CHANNEL>
+      if (userMessage[0] === '*latest') {
+        if (Object.keys(latestBans).indexOf(userMessage[1]) !== -1) {
+          if (latestBans[userMessage[1]].length) {
+            const message = `@${context['display-name']} the last banned users in ${userMessage[1]} were: ${latestBans[userMessage[1]].map((user) => `${user.username} (${msToTime(new Date() - user.time)} ago)`)}`
+            client.say(channel, `${blank ? message : message.concat(blankText)}`);
+          } else {
+            message = `@${context['display-name']} No bans registered in ${userMessage[1]} channel.`;
           }
-          client.say(channel, `@${context['display-name']} No bans were registered.`);
-          return;
+        } else {
+          message = `@${context['display-name']} This channel is not being monitored.`;
         }
-        // AYAYA
-        if (userMessage[0] === 'AYAYA') {
-          const message = 'AYAYA';
-          client.say(channel, `@${context['display-name']} ${blank ? message : message.concat(blankText)}`);
-          blank = !blank;
-          return;
-        }
-        // nyanPls
-        if (userMessage[0] === 'nyanPls') {
-          const message = '✨ nyanPls 🌸  ';
-          client.say(channel, `${blank ? message : message.concat(blankText)}`);
-          blank = !blank;
-          return;
-        }
-        // cute chat
-        if (userMessage[0] === '*cute') {
-          const message = '🌸 ✌ ️ AYAYA ✨ ⣰⠟⢷⡀⣿⠄⣿⠘⢻⡟⠃⣿⠛⠛⠄⠄⣰⠟⢷⡀⣿⠄⣿⠄⢠⣿⠄⠛⣿⠛ ⣿⠄⠄⠄⣿⠄⣿⠄⢸⡇⠄⣿⠶⠶⠄⠄⣿⠄⠄⠄⣿⠶⣿⠄⣼⣈⡇⠄⣿⠄ ⠹⣦⡾⠁⠻⣤⠟⠄⢸⡇⠄⣿⣤⣤⠄⠄⠹⣦⡾⠁⣿⠄⣿⢠⡏⠉⢻⠄⣿⠄'
-          client.say(channel, `${blank ? message : message.concat(blankText)}`);
-          blank = !blank;
-          return;
-        }
-        // paag
-        if (userMessage[0] === '*pag') {
-          const message = 'PagMan 🤘 ⣿⣯⣷⢈⣹⣿⠿⡟⢛⡛⢿⡟⠛⣿⣿⡟⠛⣿⣿⠟⠻⣿⡟⢛⡛⢿⡿⢛⡛⢿ ⣉⣛⠛⠉⠁⠄⠄⡇⢈⣁⣼⠁⠇⠸⡿⠠⠇⢹⡟⠰⠆⢻⠄⢿⡉⢹⡀⢾⡉⢹ ⡿⠌⠛⠢⣄⣀⣠⣧⣼⣿⣧⣼⣿⣤⣧⣼⣿⣤⣥⣾⣧⣬⣷⣬⣥⣼⣷⣤⣤⣼ '
-          client.say(channel, `${blank ? message : message.concat(blankText)}`);
-          blank = !blank;
-          return;
-        }
-        // join raid
-        if (userMessage[0] === '*join') {
-          console.log(channel);
-          client.say(channel, '+join');
-          return;
-        }
-        // force ed
-        if (userMessage[0] === '*fed' && context['display-name'] === process.env.USER) {
-          let timer;
-          clearInterval(timer);
-          client.say(channel, '+ed');
-          timer = startEDTimer(channel);
-        }
-        break;
-      case 2:
-        // Registered bans on <CHANNEL>
-        if (userMessage[0] === '*latest') {
-          if (Object.keys(latestBans).indexOf(userMessage[1]) !== -1) {
-            if (latestBans[userMessage[1]].length) {
-              const message = `@${context['display-name']} the last banned users in ${userMessage[1]} were: ${latestBans[userMessage[1]].map((user) => `${user.username} (${msToTime(new Date() - user.time)} ago)`)}`
-              client.say(channel, `${blank ? message : message.concat(blankText)}`);
-              blank = !blank;
-              return;
-            }
-            client.say(channel, `@${context['display-name']} No bans registered in ${userMessage[1]} channel.`);
-            return;
-          }
-          client.say(channel, `@${context['display-name']} This channel is not being monitored.`);
-          break;
-        }
-        break;
-    }
-  }, 1000);
+      }
+      break;
+  };
+  if (message) {
+    client.say(channel, `${blank ? message : message.concat(blankText)}`)
+    blank = !blank;
+    antiSpam();
+  }
+  return;
 }
 
 function onBanUserHandler(channel, username, reason, userstate) {
